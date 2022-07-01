b1379e1c4ed3e464725e79cdad3653d842df7fd1
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-07-01 17:52:34 -0300

    Aplicação pronta

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
 import React,{useState} from 'react';
-import { ImageBackground, StyleSheet, Text, View, TextInput, TouchableOpacity, Touchable } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 import Dolar from './components/Dolar';
 import Api from './components/Api';
-
-const image = {uri: "https://i.pinimg.com/736x/5d/cb/27/5dcb27e015335a776e74abc7558a5105.jpg"}
-
-
 
 export default function App() {
   const [dolar, setDolar] = useState("");
 
-
-  
   async function buscarDolar(){
     const response = await Api.get('json/last/USD-BRL');
     setDolar(response.data.USDBRL);
@@ -20,62 +14,67 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-
-<ImageBackground style={{
-position: "absolute",
-height: '100%',
-width: '100%'
-}}source={image}>
-
-</ImageBackground>
       <View style={styles.bloco}>
-      <Text style={styles.txt}> Cotação de Moedas </Text>
+        <Text style={styles.text}>Converter Dólar para Real</Text>
 
-        
-      <TouchableOpacity
-      style={styles.botao}
-      onPress={buscarDolar}
-      >
-        <Text style={styles.txtBotao}> Dólar para Real </Text>
-      </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.botao}
+          onPress={buscarDolar}
+        > 
+          <Text style={styles.txtBotao}>CONVERTER</Text>
+        </TouchableOpacity>
 
-      <Dolar data={dolar} />
-     </View>
-   </View>
+        <Dolar data={dolar} />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#5B5750',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: '30%',
   },
-  bloco: {
-    width:'100%',
-    alignItens: 'center',
+
+  bloco:{
+    fontFamily: 'sans-serif',
+    backgroundColor: '#6B7F82',
+    width: '100%',
+    alignItems:'center',
+    borderRadius: 15,
+    padding: 20,
+    borderRadius: 10,
+    borderColor: '#8EB8E5',
+    borderWidth: 5,
   },
-  texto: {
-    fontSize: 20,
-    textAlign: 'center',
+
+  text: {
+    fontSize:30,
+    color: '#492C1D',
+    fontFamily: 'sans-serif',
   },
-  botao: {
-    width:'80%',
-    fontSize: 30,
-    marginTop: 30,
-    backgroundColor: 	'#40E0D0',
-    alignItems: 'center',
-  },
-  txtBotao: {
-    fontSize:20,
-    textAlign: 'center',
-  },
-  txt: {
-     fontSize: 20,
-     textAlign: 'center',
-  },
-    image: {
-      flex: 1,
-      justifyContent: "center"
-    }
+
+botao:{
+  display: 'flex',
+  justifyContent: 'center',
+  width: 'maxContent',
+  height: 40,
+  marginVertical: 15,
+  backgroundColor:'#7C99B4',
+  borderRadius: 10,
+  borderColor: '#8EB8E5',
+  borderWidth: 5,
+  padding: 20,
+},
+
+txtBotao:{
+  fontWeight: 'bold',
+  color: '#8EB8E5',
+  fontSize:30,
+  textAlign:'center',
+}
+
 });
